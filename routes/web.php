33d0d27e0454be2f82d8ab00a9460123f5716fa3
Caique Daniel <caiquedaniel@outlook.com.br>
2023-05-24 19:37:40 -0300

    feat: Criação do template da página de artigo.

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/', function () {
     return view('pages.app.homepage.home');
 });
 
+Route::view('artigo','pages.app.post.post');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
