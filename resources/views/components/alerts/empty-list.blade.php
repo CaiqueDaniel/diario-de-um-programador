d491b6228b13c84b402078e6dafeb5f888593541
Caique Daniel <caiquedaniel@outlook.com.br>
2023-05-27 02:29:14 -0300

    feat: Ajustar listagem de posts.

---
+++ resources/views/components/alerts/empty-list.blade.php
@@ -0,0 +1,3 @@
+<div class="alert alert-info d-flex justify-content-center">
+    {{__('No items found')}}
+</div>
--- resources/views/pages/admin/post/listing.blade.php
+++ resources/views/pages/admin/post/listing.blade.php
@@ -1,45 +1,57 @@
 @extends('layouts.admin')
 
 @section('content')
+    @php
+        /**
+        * @var \Illuminate\Pagination\LengthAwarePaginator $response
+        * @var \App\Models\Post[] $items
+        */
+        $items = $response->items();
+    @endphp
+
     <div class="container">
         <x-alerts.success/>
 
         <div class="listing">
-            <ul class="list-group list-group-horizontal header mb-3 d-none d-md-flex sticky-top">
-                <li class="list-group-item col-5">{{__('Title')}}</li>
-                <li class="list-group-item col-3">{{__('Author')}}</li>
-                <li class="list-group-item col-2">{{__('Published At')}}</li>
-                <li class="list-group-item col-2">{{__('Actions')}}</li>
-            </ul>
-
-            @foreach($response->items() as $item)
-                <ul class="list-group list-group-horizontal-md mb-1">
-                    <li class="list-group-item col-md-5">
-                        <b class="d-md-none">{{__('Title')}}: </b>{{$item->title}}
-                    </li>
-
-                    <li class="list-group-item col-md-3">
-                        <b class="d-md-none">{{__('Author')}}: </b>{{$item->getRelation('author')->name}}
-                    </li>
-
-                    <li class="list-group-item col-md-2">
-                        <b class="d-md-none">{{__('Published At')}}: </b>{{$item->created_at}}
-                    </li>
-
-                    <li class="list-group-item col-md-2 d-flex align-items-center justify-content-evenly">
-                        <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" role="switch"/>
-                        </div>
-
-                        <a class="btn btn-sm btn-outline-warning" href="{{route('admin.post.edit', $item->id)}}">
-                            <i class="fa-regular fa-pen-to-square"></i>
-                        </a>
-                        <a class="btn btn-sm btn-outline-danger">
-                            <i class="fa-solid fa-trash"></i>
-                        </a>
-                    </li>
+            @empty($items)
+                <x-alerts.empty-list/>
+            @else
+                <ul class="list-group list-group-horizontal header mb-3 d-none d-md-flex sticky-top">
+                    <li class="list-group-item col-5">{{__('Title')}}</li>
+                    <li class="list-group-item col-3">{{__('Author')}}</li>
+                    <li class="list-group-item col-2">{{__('Published At')}}</li>
+                    <li class="list-group-item col-2">{{__('Actions')}}</li>
                 </ul>
-            @endforeach
+
+                @foreach($items as $item)
+                    <ul class="list-group list-group-horizontal-md mb-1">
+                        <li class="list-group-item col-md-5">
+                            <b class="d-md-none">{{__('Title')}}: </b>{{$item->title}}
+                        </li>
+
+                        <li class="list-group-item col-md-3">
+                            <b class="d-md-none">{{__('Author')}}: </b>{{$item->getRelation('author')->name}}
+                        </li>
+
+                        <li class="list-group-item col-md-2">
+                            <b class="d-md-none">{{__('Published At')}}: </b>{{$item->created_at}}
+                        </li>
+
+                        <li class="list-group-item col-md-2 d-flex align-items-center justify-content-evenly">
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" role="switch"/>
+                            </div>
+
+                            <a class="btn btn-sm btn-outline-warning" href="{{route('admin.post.edit', $item->id)}}">
+                                <i class="fa-regular fa-pen-to-square"></i>
+                            </a>
+                            <a class="btn btn-sm btn-outline-danger">
+                                <i class="fa-solid fa-trash"></i>
+                            </a>
+                        </li>
+                    </ul>
+                @endforeach
+            @endempty
         </div>
 
         <div class="d-flex justify-content-center">
