7ca416d8dfa7f76875f5876f83eccd37ded485fa
Caique Daniel <caiquedaniel@outlook.com.br>
2023-06-04 21:48:53 -0300

    feat: Adicionar model Category

---
+++ app/Models/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $permalink
+ * @property string $deleted_at
+ */
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $timestamps = false;
+    protected $fillable = ['name'];
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Category::class, 'parent');
+    }
+}
