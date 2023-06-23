9c7d29f16c3d665de88135f2ed1d9e7235670093
Caique Daniel <caiquedaniel@outlook.com.br>
2023-06-23 00:30:18 -0300

    dist: Configurar seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->create([
+            'name' => 'Teste',
+            'email' => 'teste@gmail.com',
+            'password' => Hash::make('123456')
+        ]);
+    }
+}
