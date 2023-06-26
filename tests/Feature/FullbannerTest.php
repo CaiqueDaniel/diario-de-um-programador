023fa756986d1c40d1202b9436f77730b3fb93c9
Caique Daniel <caiquedaniel@outlook.com.br>
2023-06-25 23:14:48 -0300

    test: Implementar tests do módulo de fullbanner

---
+++ tests/Feature/FullbannerTest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\{FullBanner, User};
+use Illuminate\Foundation\Testing\{RefreshDatabase, WithFaker};
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Hash;
+use Tests\Feature\Interfaces\{CRUDTest, SoftDeleteTest};
+use Tests\TestCase;
+
+class FullbannerTest extends TestCase implements CRUDTest, SoftDeleteTest
+{
+    use RefreshDatabase, WithFaker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->migrateFreshUsing();
+
+        $user = new User([
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'password' => Hash::make('123456'),
+        ]);
+
+        $user->save();
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => '123456'
+        ]);
+    }
+
+    public function test_listing(): void
+    {
+        $response = $this->get('/painel/fullbanners/listar');
+
+        $response
+            ->assertOk()
+            ->assertViewIs('pages.admin.fullbanner.listing');
+    }
+
+    public function test_load_form_create(): void
+    {
+        $response = $this->get('/painel/fullbanners/adicionar');
+
+        $response
+            ->assertOk()
+            ->assertViewIs('pages.admin.fullbanner.form');
+    }
+
+    public function test_creation(): void
+    {
+        $title = $this->faker->name();
+        $link = $this->faker->url();
+        $position = 1;
+
+        $response = $this->post('/painel/fullbanners', [
+            'title' => $title,
+            'link' => $link,
+            'image' => UploadedFile::fake()->create($this->faker->name() . '.jpg'),
+            'position' => $position
+        ]);
+
+        $response
+            ->assertRedirect()
+            ->assertSessionHas('message', 'Fullbanner criado com sucesso');
+
+        $this->assertDatabaseHas(FullBanner::class, ['title' => $title, 'link' => $link, 'postion' => $position]);
+
+        /** @var FullBanner $banner */
+        $banner = FullBanner::query()->where('title', 'like', $title)->first();
+
+        $this->assertNotNull($banner->image);
+    }
+
+    public function test_deletion(): void
+    {
+        $banner = new FullBanner([
+            'title' => $this->faker->name(),
+            'link' => $this->faker->url(),
+            'image' => UploadedFile::fake()->create($this->faker->name() . '.jpg'),
+            'position' => 1
+        ]);
+
+        $banner->save();
+
+        $this->assertDatabaseHas(FullBanner::class, $banner->toArray());
+
+        $response = $this->delete("/painel/fullbanners/{$banner->id}");
+
+        $response
+            ->assertRedirect()
+            ->assertSessionHas('message', 'Fullbanner excluído com sucesso');
+
+        $this->assertDatabaseMissing(FullBanner::class, $banner->toArray());
+    }
+
+    public function test_update(): void
+    {
+        $banner = new FullBanner([
+            'title' => $this->faker->name(),
+            'link' => $this->faker->url(),
+            'image' => UploadedFile::fake()->create($this->faker->name() . '.jpg'),
+            'position' => 1
+        ]);
+
+        $banner->save();
+
+        $this->assertDatabaseHas(FullBanner::class, $banner->toArray());
+
+        $title = $this->faker->name();
+        $link = $this->faker->url();
+        $position = 2;
+
+        $response = $this->put("/painel/fullbanners/{$banner->id}", [
+            'title' => $title,
+            'link' => $link,
+            'image' => UploadedFile::fake()->create($this->faker->name() . '.jpg'),
+            'position' => $position
+        ]);
+
+        $response
+            ->assertRedirect()
+            ->assertSessionHas('message', 'Fullbanner alterado com sucesso');
+
+        $this->assertDatabaseHas(FullBanner::class, ['title' => $title, 'link' => $link, 'postion' => $position]);
+
+        /** @var FullBanner $banner */
+        $banner = FullBanner::query()->where('title', 'like', $title)->first();
+
+        $this->assertNotNull($banner->image);
+    }
+
+    public function test_enabling_item(): void
+    {
+        // TODO: Implement test_enabling_item() method.
+    }
+
+    public function test_disabling_item(): void
+    {
+        // TODO: Implement test_disabling_item() method.
+    }
+}
