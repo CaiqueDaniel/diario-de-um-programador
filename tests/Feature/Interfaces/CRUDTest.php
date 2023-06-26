2eb1dc444ebcf65c6f34f450c955857d86561c77
Caique Daniel <caiquedaniel@outlook.com.br>
2023-06-25 23:03:22 -0300

    test: Criar interfaces com metodo de testes de CRUD.

---
+++ tests/Feature/Interfaces/CRUDTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tests\Feature\Interfaces;
+
+interface CRUDTest extends ListTest, LoadFormTest, CreateTest, UpdateTest, DeleteTest
+{
+
+}
---
+++ tests/Feature/Interfaces/DeleteTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tests\Feature\Interfaces;
+
+interface DeleteTest
+{
+    public function test_deletion(): void;
+}
