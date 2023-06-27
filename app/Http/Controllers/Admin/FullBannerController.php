5ba75a00ee9432fa2ee67bce292d24be5a67ccd9
Caique Daniel <caiquedaniel@outlook.com.br>
2023-06-26 21:07:34 -0300

    feat: Implementar rota de exclusão de fullbanners

--- app/Http/Controllers/Admin/FullBannerController.php
+++ app/Http/Controllers/Admin/FullBannerController.php
@@ -56,14 +56,12 @@ class FullBannerController extends Controller
         return redirect()->route('admin.fullbanner.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(FullBanner $fullbanner): RedirectResponse
     {
-        //
+        $fullbanner->forceDelete();
+
+        session()->flash('message', 'Fullbanner excluído com sucesso');
+
+        return redirect()->route('admin.fullbanner.index');
     }
 }
