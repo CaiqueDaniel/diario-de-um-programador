5c542b5f83f03cc8ef4be349c5c567933807afd2
Caique Daniel <caiquedaniel@outlook.com.br>
2023-07-03 19:39:50 -0300

    feat: Adicionar suporte a outras linguagens

--- resources/js/web/pages/posts/components/CodeParser.ts
+++ resources/js/web/pages/posts/components/CodeParser.ts
@@ -6,8 +6,14 @@ import 'prismjs/components/prism-markup-templating'
 
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-php';
 import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-bash';
 
 import 'prismjs/plugins/toolbar/prism-toolbar';
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
