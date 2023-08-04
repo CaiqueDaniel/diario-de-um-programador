ac3bfc9c8284ab6a006457a8527bf350b0f06bd3
Caique Daniel <caiquedaniel@outlook.com.br>
2023-08-04 00:12:53 -0300

    feat: adicionar link e titulo do banner

--- resources/views/components/full-banner-carousel.blade.php
+++ resources/views/components/full-banner-carousel.blade.php
@@ -1,9 +1,28 @@
+@php
+    /**
+     * @var \Illuminate\Support\Collection<int,\App\Models\FullBanner> $fullbanners
+     * @var \App\Models\FullBanner $banner
+     */
+@endphp
+
 <div id="fullbanner" class="carousel slide">
     <div class="carousel-inner">
         @foreach($fullbanners as $index => $banner)
             <div class="carousel-item {{$index==0 ? 'active' : ''}}">
-                <img src="{{asset("storage/{$banner->getImage()}")}}" class="d-block w-100" loading="lazy"
-                     alt="{{$banner->getTitle()}}" style="max-height: 400px; object-fit: cover;">
+                <a href="{{$banner->getLink()}}" title="{{$banner->getTitle()}}" class="text-decoration-none">
+                    <img src="{{asset("storage/{$banner->getImage()}")}}" class="d-block w-100" loading="lazy"
+                         alt="{{$banner->getTitle()}}" style="height: 400px; object-fit: cover;">
+
+                    <div class="
+                        hud-cover
+                        position-absolute top-0
+                        w-100 h-100
+                        d-flex flex-column justify-content-center align-items-center
+                        px-2
+                    ">
+                        <h3 class="text-white">{{$banner->getTitle()}}</h3>
+                    </div>
+                </a>
             </div>
         @endforeach
     </div>
