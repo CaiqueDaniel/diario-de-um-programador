d21965597cab9432a7b180f5b9c459273c482656
Caique Daniel <caiquedaniel@outlook.com.br>
2023-09-04 23:28:08 -0300

    feat: ajustes visuais na search bar

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,14 +1,17 @@
 <nav class="navbar navbar-expand-lg bg-light border rounded mb-5">
     <div class="container-fluid">
         <form class="d-flex" role="search" method="GET" action="">
-            <input class="form-control me-2" type="search" name="search" placeholder="{{__('Search')}}" aria-label="{{__('Search')}}">
-            <button class="btn btn-outline-success" type="submit">{{__('Search')}}</button>
+            <input class="form-control me-2" type="search" name="search" placeholder="{{__('Search')}}"
+                   aria-label="{{__('Search')}}">
+            <button class="btn btn-outline-success" type="submit">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
         </form>
 
         <a href="{{$add}}" class="text-decoration-none">
             <div class="d-flex flex-column align-items-center">
                 <i class="fa-regular fa-plus"></i>
-                <span>{{__('Add')}}</span>
+                <span class="d-none d-md-block">{{__('Add')}}</span>
             </div>
         </a>
     </div>
