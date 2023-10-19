b0ccc7ded161b58a344e5b7ac45c5046c23a9eb3
Caique Daniel <caiquedaniel@outlook.com.br>
2023-10-19 00:17:01 -0300

    feat: remover required dos campos de thumbnail e subtítulo

--- resources/views/components/posts/item-post.blade.php
+++ resources/views/components/posts/item-post.blade.php
@@ -9,8 +9,13 @@
 <div class="col-md-4 row justify-content-center mb-2 mt-2">
     <div class="card col-md-11 p-0">
         <a href="{{route('web.post.view',['slug' => $item->getPermalink()])}}" class="text-decoration-none">
-            <img src="{{asset("storage/{$item->getThumbnail()}")}}" class="card-img-top" alt="{{$item->getTitle()}}"
-                 loading="lazy" style="min-height:300px; object-fit: cover">
+            @empty($item->getThumbnail())
+                <img src="{{asset("assets/default-post-cover-list.png")}}" class="card-img-top" alt="{{$item->getTitle()}}"
+                     loading="lazy" style="min-height:300px; object-fit: cover">
+            @else
+                <img src="{{asset("storage/{$item->getThumbnail()}")}}" class="card-img-top" alt="{{$item->getTitle()}}"
+                     loading="lazy" style="min-height:300px; object-fit: cover">
+            @endempty
             <div class="card-body">
                 @if($categories->isNotEmpty())
                     <span class="badge rounded-pill text-bg-secondary mb-2">
