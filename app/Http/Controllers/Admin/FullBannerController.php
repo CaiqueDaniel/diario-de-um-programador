198d17b0812614bb9a19be72b928b82c2dd612d0
Caique Daniel <caiquedaniel@outlook.com.br>
2023-10-29 03:31:43 -0300

    refactor: refatorar módulo de remoção de fullbanner

--- app/Http/Controllers/Admin/FullBannerController.php
+++ app/Http/Controllers/Admin/FullBannerController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Actions\Fullbanner\CreateFullbannerAction;
+use App\Actions\Fullbanner\DeleteFullbannerAction;
 use App\Actions\Fullbanner\UpdateFullbannerAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Get\SearchRequest;
 use App\Http\Requests\Post\FullBannerRequest;
 use App\Models\FullBanner;
-use App\Services\FullBannerService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
@@ -16,13 +16,6 @@ use Throwable;
 
 class FullBannerController extends Controller
 {
-    private FullBannerService $fullBannerService;
-
-    public function __construct(FullBannerService $fullBannerService)
-    {
-        $this->fullBannerService = $fullBannerService;
-    }
-
     public function index(SearchRequest $request): View
     {
         $response = FullBanner::findAll($request->get('search'));
@@ -63,9 +56,9 @@ class FullBannerController extends Controller
         return redirect()->route('admin.fullbanner.index');
     }
 
-    public function destroy(FullBanner $fullbanner): RedirectResponse
+    public function destroy(FullBanner $fullbanner, DeleteFullbannerAction $deleteFullbanner): RedirectResponse
     {
-        $this->fullBannerService->destroy($fullbanner);
+        $deleteFullbanner->execute($fullbanner);
 
         session()->flash('message', 'Fullbanner excluído com sucesso');
 
